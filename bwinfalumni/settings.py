c0e51f79876412d3cb066371f5a9a5406d454df7
Robert Czechowski <robert+git@czecho.de>
2017-05-20 20:05:00 +0200

    rename MIDDLEWARE to -MIDDLEWARE_CLASSES (seems to be a django change)

--- bwinfalumni/settings.py
+++ bwinfalumni/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
