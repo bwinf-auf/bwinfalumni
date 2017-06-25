6aa9e06722b2b604b4db5c33763675fccae495f9
Robert Czechowski <robert+git@czecho.de>
2017-06-25 19:17:34 +0200

    Versuche Mockdaten-Skript zu beschleunigen

--- mockverein/__init__.py
+++ mockverein/__init__.py
@@ -5,57 +5,55 @@ from umsaetze.models import Umsatz, UmsatzTyp, Konto
 
 
 def erstelle_mockdaten():
-    testuser1 = User.objects.create_user('test', 'test@example.com', 'test', is_staff=True)
-    testuser2 = User.objects.create_user('max', 'max@example.com', 'max', is_staff=True)
-    testuser3 = User.objects.create_user('willi', 'willi@example.com', 'willi', is_staff=True)
-    testuser4 = User.objects.create_user('williadmin', 'willi@example.com', 'willi', is_staff=True)
-    testuser5 = User.objects.create_user('nico', 'nico@example.com', 'nico', is_staff=True)
-    testuser1.save()
-    testuser2.save()
-    testuser3.save()
-    testuser4.save()
-    testuser5.save()
+    testusers = [
+        User.objects.create_user('test', 'test@example.com', 'test', is_staff=True),
+        User.objects.create_user('max', 'max@example.com', 'max', is_staff=True),
+        User.objects.create_user('willi', 'willi@example.com', 'willi', is_staff=True),
+        User.objects.create_user('williadmin', 'willi@example.com', 'willi', is_staff=True),
+        User.objects.create_user('nico', 'nico@example.com', 'nico', is_staff=True),
+        ]
     
-    testmitglied1 = Mitglied(mitgliedsnummer=99927, vorname="Test", nachname="Testofsky", strasse="Str", plz="12343", stadt="B.", telefon="0", email="test@example.com") 
-    testmitglied2 = Mitglied(mitgliedsnummer=99928, vorname="Max", nachname="Mustermann", strasse="Str", plz="12343", stadt="B.", telefon="0", email="max@example.com")
-    testmitglied3 = Mitglied(mitgliedsnummer=99929, vorname="Willi", nachname="Vanilly", strasse="Str", plz="12343", stadt="B.", telefon="0", email="willi@example.com")
-    testmitglied4 = Mitglied(mitgliedsnummer=99930, vorname="Lena", nachname="Lane", strasse="Str", plz="12343", stadt="B.", telefon="0", email="lena@example.com")
-    testmitglied1.save()
-    testmitglied2.save()
-    testmitglied3.save()
-    testmitglied4.save()
+    testmitglieder = [
+        Mitglied(mitgliedsnummer=99927, vorname="Test", nachname="Testofsky", strasse="Str", plz="12343", stadt="B.", telefon="0", email="test@example.com"),
+        Mitglied(mitgliedsnummer=99928, vorname="Max", nachname="Mustermann", strasse="Str", plz="12343", stadt="B.", telefon="0", email="max@example.com"),
+        Mitglied(mitgliedsnummer=99929, vorname="Willi", nachname="Vanilly", strasse="Str", plz="12343", stadt="B.", telefon="0", email="willi@example.com"),
+        Mitglied(mitgliedsnummer=99930, vorname="Lena", nachname="Lane", strasse="Str", plz="12343", stadt="B.", telefon="0", email="lena@example.com"),
+        ]
     
-    bm1 = BenutzerMitglied(benutzer=testuser1, mitglied=testmitglied1)
-    bm2 = BenutzerMitglied(benutzer=testuser2, mitglied=testmitglied2)
-    bm3 = BenutzerMitglied(benutzer=testuser3, mitglied=testmitglied3)
-    bm4 = BenutzerMitglied(benutzer=testuser4, mitglied=testmitglied3)
-    bm1.save()
-    bm2.save()
-    bm3.save()
-    bm4.save()
+    bm = [
+        BenutzerMitglied(benutzer=testusers[0], mitglied=testmitglieder[0]),
+        BenutzerMitglied(benutzer=testusers[1], mitglied=testmitglieder[1]),
+        BenutzerMitglied(benutzer=testusers[2], mitglied=testmitglieder[2]),
+        BenutzerMitglied(benutzer=testuser[3], mitglied=testmitglieder[2]),
+        ]
     
     konto = Konto(kontoname="Girokonto", beschreibung="Bei Deutscher Bank")
     konto.save()
     
-    ut1 = UmsatzTyp(typname="Einzahlung", beschreibung="Einzahlung Mitgliedsbeitrag")
-    ut2 = UmsatzTyp(typname="Rechnung", beschreibung="Begleichung von Rechnung")
-    ut1.save()
-    ut2.save()
+    ut = [
+        UmsatzTyp(typname="Einzahlung", beschreibung="Einzahlung Mitgliedsbeitrag"),
+        UmsatzTyp(typname="Rechnung", beschreibung="Begleichung von Rechnung"),
+        ]
     
-    umsatz = Umsatz(konto=konto,typ=ut2,text="Jahrebeitrag DJH", centWert=12203,rechnung="blub",geschaeftspartner="DJH")
+    umsatz = Umsatz(konto=konto,typ=ut[1],text="Jahrebeitrag DJH", centWert=12203,rechnung="blub",geschaeftspartner="DJH")
     umsatz.save()
     
-    mkbt1 = MitgliedskontoBuchungstyp(typname="Einzahlung")
-    mkbt2 = MitgliedskontoBuchungstyp(typname="Gutschrift")
-    mkbt3 = MitgliedskontoBuchungstyp(typname="Mitgliedsbeitrag 2017")
-    mkbt1.save()
-    mkbt2.save()
-    mkbt3.save()
+    mkbt = [
+        MitgliedskontoBuchungstyp(typname="Einzahlung"),
+        MitgliedskontoBuchungstyp(typname="Gutschrift"),
+        MitgliedskontoBuchungstyp(typname="Mitgliedsbeitrag 2017"),
+        ]
 
-    mkb1 = MitgliedskontoBuchung(mitglied=testmitglied2,typ=mkbt2,centWert=-10)
-    mkb2 = MitgliedskontoBuchung(mitglied=testmitglied2,typ=mkbt1,centWert=10)
-    mkb3 = MitgliedskontoBuchung(mitglied=testmitglied2,typ=mkbt3,centWert=-10)
-    mkb1.save()
-    mkb2.save()
-    mkb3.save()
-    
+
+    mkb = [
+        MitgliedskontoBuchung(mitglied=testmitglieder[1],typ=mkbt[1],centWert=-10),
+        MitgliedskontoBuchung(mitglied=testmitglieder[1],typ=mkbt[0],centWert=10),
+        MitgliedskontoBuchung(mitglied=testmitglieder[1],typ=mkbt[2],centWert=-10),
+        ]
+
+    User.objects.bulk_create(testusers)
+    Mitglied.objects.bulk_create(testmitglieder)
+    BenutzerMitglied.objects.bulk_create(bm)
+    UmsatzTyp.objects.bulk_create(ut)
+    MitgliedskontoBuchungstyp.objects.bulk_create(mkbt)
+    MitgliedskontoBuchung.objects.bulk_create(mkb)
