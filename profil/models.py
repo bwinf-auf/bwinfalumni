28d3b13b39a81a9d902b34e646195b9053be2bf0
Robert Czechowski <robert+git@czecho.de>
2017-06-25 16:15:30 +0200

    Ändere Struktur von Profil-Sichtbarkeitseinstellungen

--- profil/models.py
+++ profil/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from mitglieder.models import Mitglied
 
-# Create your models here.
+
+class Sichtbarkeit(models.Model):
+    mitglied = models.ForeignKey(Mitglied, on_delete=models.PROTECT)
+    bereich  = models.CharField(max_length=200)
+    sache    = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return str(self.mitglied.mitgliedsnummer)+ " (" + str(self.bereich) + "): " + str(self.sache)
+    
+    class Meta:
+        db_table = "sichtbarkeit"
+        verbose_name = "Sichtbarkeit"
+        verbose_name_plural = "Sichtbarkeiten"
--- profil/urls.py
+++ profil/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'profil'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^sichtbarkeit/$', views.sichtbarkeit, name='sichtbarkeit'),
     #url(r'^(?P<userid>[0-9]+)/$', views.showuser, name='detail'),
 ]
