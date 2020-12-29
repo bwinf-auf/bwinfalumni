feccd60dbd3ee7374357b902a626ff4d459f154a
Nikolai Wyderka <nikolai@wyderka.de>
2020-12-29 15:42:07 +0100

    Neue App "Kontostatistik" zur Anzeige der schlimmsten Mitgliedbeitragssünder und des Gesamtmitgliedbeitragsaldos.

---
+++ kontostatistik/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class KontostatistikConfig(AppConfig):
+    name = 'kontostatistik'
---
+++ kontostatistik/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, user_passes_test
+
+from mitglieder.models import Mitglied, MitgliedskontoBuchung, MitgliedskontoBuchungstyp
+
+# Create your views here.
+
+@login_required
+@user_passes_test(lambda u: u.is_superuser or u.groups.filter(name='vorstand').exists())
+def index(request):
+    # Calculate Mitgliedskonto-Saldo
+    #benutzer = request.user
+    #try:
+        #mitglied = benutzer.benutzermitglied.mitglied
+    #except:
+        #raise Http404("Keine Benutzerinformationen vorhanden.")
+    
+
+    all_mitglieder = Mitglied.objects.order_by('mitgliedsnummer')
+    
+    values = {}
+    for mitglied in all_mitglieder:
+        val = 0
+        for buchung in mitglied.mitgliedskontobuchung_set.all().order_by('buchungsdatum'):
+            val += buchung.cent_wert
+        values[mitglied.mitgliedsnummer] = {'mitglied': mitglied, 'betrag': val/100.0}
+    
+    
+    value_list = [v for v in values.values()]
+    value_list = sorted(value_list, key=lambda x: x['betrag'], reverse=False)
+    
+    saldo = sum([v['betrag'] for v in value_list])
+    
+    return render(request, 'kontostatistik/kontostatistik.html', {'values': value_list, 'saldo': saldo})
+
