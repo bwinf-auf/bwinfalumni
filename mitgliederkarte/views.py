35dcd4417be8141addb0dcd8842860766ef94757
Robert Czechowski <roczech+git@gmail.com>
2021-04-12 23:38:51 +0200

    Profil: Umbennenen der Sichtbarkeit 'wohnort' in 'karte' und entsprechenden Anpassungen in den Templates

--- mitgliederkarte/views.py
+++ mitgliederkarte/views.py
@@ -6,18 +6,16 @@ from mitglieder.models import Mitglied
 import math
 import datetime
 
-# Create your views here.
-
 plzs = None
 
 def load_plz():
     global plzs
-    
+
     if plzs is not None:
         return
-    
+
     plzs = {}
-    
+
     try:
         with open("mitgliederkarte/data/PLZ.tab", "r") as f:
             for line in f:
@@ -30,80 +28,69 @@ def load_plz():
                 plzs[plz] = (lon, lat)
     except:
         plzs = None
-    
 
 
 def plz_to_coord(plz):
     global plzs
-    
+
     load_plz()
-    
+
     if plzs is None:
         return
-    
+
     if not plz in plzs:
         return None
     else:
         return plzs[plz]
-    
 
-#@login_required
-#@user_passes_test(lambda u: u.is_superuser or u.groups.filter(name='vorstand').exists())
+
 def index(request):
     is_authenticated = request.user.is_authenticated
-    
+
     try:
         ownmitglied = request.user.benutzermitglied.mitglied
     except:
         ownmitglied = None
-    
-    
+
     bereich = "welt" if not is_authenticated else "alumni"
-    
-    plzmitglieder = {}
-    
-    mitgliedercoords = []
-    
-    wohnort_mitglieder = Mitglied.objects.filter(sichtbarkeit__bereich=bereich, sichtbarkeit__sache='wohnort')
-    adresse_mitglieder = Mitglied.objects.filter(sichtbarkeit__bereich=bereich, sichtbarkeit__sache='adresse')
-    
+
     today = datetime.datetime.today()
-    
-    all_mitglieder = wohnort_mitglieder.union(adresse_mitglieder).filter(beitrittsdatum__lte=today).exclude(austrittsdatum__lte=today)
-    
-    for mitglied in all_mitglieder:
+    karte_mitglieder = Mitglied.objects.filter(sichtbarkeit__bereich=bereich, sichtbarkeit__sache='karte')
+    karte_mitglieder = karte_mitglieder.filter(beitrittsdatum__lte=today).exclude(austrittsdatum__lte=today)
+
+    plzmitglieder = {}
+    mitgliedercoords = []
+
+    for mitglied in karte_mitglieder:
         plz = mitglied.plz
         land = mitglied.land
-        
+
         if land.lower() not in ["de", "deutschland"]:
             continue
-        
+
         if not plz in plzmitglieder:
             plzmitglieder[plz] = [mitglied]
         else:
             plzmitglieder[plz].append(mitglied)
-    
+
     for plz in plzmitglieder:
         coords = plz_to_coord(plz)
         if coords is None:
             continue
-        
+
         num = len(plzmitglieder[plz])
         delta = math.sqrt(num-1)*0.01
-        
+
         for i in range(len(plzmitglieder[plz])):
             m = plzmitglieder[plz][i]
-            
+
             mlon = coords[0] + delta*math.cos(2.*math.pi * i/num)
             mlat = coords[1] + delta*math.sin(2.*math.pi * i/num)
-        
-            mtext = [m.vorname, m.nachname, m.plz, m.stadt, "/profil/"+str(m.mitgliedsnummer)]
-            
-            do_highlight = (ownmitglied is not None and m.mitgliedsnummer == ownmitglied.mitgliedsnummer)
-            
-            mitgliedercoords.append( [do_highlight, mlon, mlat, mtext]  )
-        
-    
-    
-    return render(request, 'mitgliederkarte/karte.html', {'coords': mitgliedercoords})
 
+            mtext = [m.vorname, m.nachname, m.plz, m.stadt, "/profil/"+str(m.mitgliedsnummer)]
+
+            do_highlight = (ownmitglied is not None and m.mitgliedsnummer == ownmitglied.mitgliedsnummer)
+
+            mitgliedercoords.append( [do_highlight, mlon, mlat, mtext]  )
+
+    return render(request, 'mitgliederkarte/karte.html', {'coords': mitgliedercoords})
--- profil/views.py
+++ profil/views.py
@@ -35,8 +35,8 @@ sichtbarkeiten   = [("alumni", "vorname"),
                     ("welt", "email"),
                     ("welt", "telefon"),
                     ("welt", "adresse"),
-                    ("alumni", "wohnort"),
-                    ("welt", "wohnort"),
+                    ("alumni", "karte"),
+                    ("welt", "karte"),
                     ]
 
 
@@ -53,10 +53,10 @@ def index(request):
         mitglied = benutzer.benutzermitglied.mitglied
     except:
         raise Http404("Keine Benutzerinformationen vorhanden.")
-    
+
     errormessage = ""
     successmessage = ""
-    
+
     if request.method == 'POST':
         form = MitgliedForm(instance=mitglied, data=request.POST)
         if form.is_valid():
@@ -66,18 +66,18 @@ def index(request):
             errormessage = "Es sind Fehler aufgetreten."
     else:
         form = MitgliedForm(instance=mitglied)
-        
-        
+
+
     sichtbar = [Sichtbarkeit.objects.filter(mitglied=mitglied)
                                     .filter(bereich=sb[0])
                                     .filter(sache=sb[1])
                                     .exists() for sb in sichtbarkeiten]
-    
+
     return render(request, 'profil/profil.html', {'form': form,
-                                                  'mitglied': mitglied, 
+                                                  'mitglied': mitglied,
                                                   'errormessage': errormessage,
-                                                  'successmessage': successmessage, 
-                                                  'sichtbar': sichtbar})  
+                                                  'successmessage': successmessage,
+                                                  'sichtbar': sichtbar})
 
 
 def showuser(request, mitgliedid):
@@ -87,7 +87,7 @@ def showuser(request, mitgliedid):
     mitglied = get_object_or_404(Mitglied, mitgliedsnummer__exact = mitgliedid)
     info = {}
     keine_info = True
-    for sache in ["vorname", "nachname", "studienort", "studienfach", "beruf", "email", "telefon", "adresse", "wohnort"]:
+    for sache in ["vorname", "nachname", "studienort", "studienfach", "beruf", "email", "telefon", "adresse", "karte"]:
         if sichtbar(mitglied, scope, sache):
             keine_info = False
             info[sache] = True
@@ -98,7 +98,7 @@ def showuser(request, mitgliedid):
     info['email_sanitised'] = mitglied.email.replace("@", " [ bei ] ")
     info["mitglied"] = mitglied
     info["initiale"] = mitglied.vorname[0].upper()
-    
+
     return render(request, 'profil/anzeige.html', {'info': info})
 
 def sichtbar(mitglied, bereich, sache):
@@ -107,14 +107,14 @@ def sichtbar(mitglied, bereich, sache):
 def showallusers(request):
     is_authenticated = request.user.is_authenticated
     scope = "alumni" if is_authenticated else "welt"
-    
+
     today = date.today()
     mitglieder = Mitglied.objects.filter(beitrittsdatum__lte = today).exclude(austrittsdatum__lte = today).order_by('vorname')
     infos = []
     for mitglied in mitglieder:
         info = {}
         keine_info = True
-        for sache in ["vorname", "nachname", "studienort", "studienfach", "beruf", "email", "telefon", "adresse", "wohnort"]:
+        for sache in ["vorname", "nachname", "studienort", "studienfach", "beruf", "email", "telefon", "adresse", "karte"]:
             if sichtbar(mitglied, scope, sache):
                 keine_info = False
                 info[sache] = True
@@ -126,7 +126,7 @@ def showallusers(request):
 
 class SichtbarkeitForm(forms.Form):
     sichtbarkeit = forms.BooleanField(required=False)
-    
+
 @login_required
 def sichtbarkeit(request):
     benutzer = request.user
@@ -134,12 +134,12 @@ def sichtbarkeit(request):
         mitglied = benutzer.benutzermitglied.mitglied
     except:
         raise Http404("Keine Benutzerinformationen vorhanden.")
-    
+
     errormessage = ""
     successmessage = ""
-    
-    SichtbarkeitFormSet = formset_factory(SichtbarkeitForm, extra=15)
-    
+
+    SichtbarkeitFormSet = formset_factory(SichtbarkeitForm)
+
     if request.method == 'POST':
         form = SichtbarkeitFormSet(data=request.POST)
         if form.is_valid():
@@ -153,20 +153,19 @@ def sichtbarkeit(request):
                         neu = Sichtbarkeit.objects.create(mitglied=mitglied,
                                                           bereich=sichtbarkeit[0],
                                                           sache=sichtbarkeit[1])
-                
+
                 i += 1
-            
+
             successmessage = "Einstellungen aktualisiert. Bitte überprüfe, ob deine Daten in der Mitgliederliste richtig angezeigt werden."
         else:
             errormessage = "Es sind Fehler aufgetreten."
     else:
-        initial = []
+        initial = [] # array of dicts with boolean value for the checkboxes
         for sichtbarkeit in sichtbarkeiten:
             initial.append({'sichtbarkeit': Sichtbarkeit.objects.filter(mitglied=mitglied)
                                                         .filter(bereich=sichtbarkeit[0])
                                                         .filter(sache=sichtbarkeit[1])
                                                         .exists()})
         form = SichtbarkeitFormSet(initial=initial)
-    
-    return render(request, 'profil/sichtbarkeit.html', {'form': form, 'mitglied': mitglied, 'errormessage': errormessage, 'successmessage': successmessage})  
 
+    return render(request, 'profil/sichtbarkeit.html', {'form': form, 'mitglied': mitglied, 'errormessage': errormessage, 'successmessage': successmessage})
