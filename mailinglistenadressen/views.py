5fac9be625fe109178edbb8a6befc69dd52ffa2a
Robert Czechowski <roczech+git@gmail.com>
2021-04-20 21:27:34 +0200

    Mailinglistenadressen: Erzeuge ECHTES csv

--- mailinglistenadressen/views.py
+++ mailinglistenadressen/views.py
@@ -1,46 +1,49 @@
 from django.shortcuts import render
-from mitglieder.models import Mitglied
 from django.http import HttpResponse
+
+from mitglieder.models import Mitglied
+
+import csv
 import datetime
+
 
 def index(request):
     today = datetime.datetime.today()
-    
+
     with open('listen/mitgliederliste', 'w', encoding='utf8') as f:
-    
-        m = Mitglied.objects \
+        ms = Mitglied.objects \
                     .filter(sichtbarkeit__bereich='alumni', sichtbarkeit__sache='mailingliste') \
                     .filter(beitrittsdatum__lte=today) \
                     .exclude(austrittsdatum__lte=today)
-            
-        for mitglied in m:
+
+        for mitglied in ms:
             f.write(mitglied.email + "\n")
-            
+
     with open('listen/ankuendigungenliste', 'w', encoding='utf8') as f:
-    
-        m = Mitglied.objects \
+        ms = Mitglied.objects \
                     .filter(beitrittsdatum__lte=today) \
                     .exclude(austrittsdatum__lte=today)
-            
-        for mitglied in m:
+
+        for mitglied in ms:
             f.write(mitglied.email + "\n")
-    
-    with open('listen/bwinfadressen', 'w', encoding='utf8') as f:
-    
-        m = Mitglied.objects \
+
+    with open('listen/bwinfadressen.csv', 'w', encoding='utf8', newline='') as f:
+        ms = Mitglied.objects \
                     .filter(sichtbarkeit__bereich='bwinf', sichtbarkeit__sache='vorname') \
                     .filter(sichtbarkeit__bereich='bwinf', sichtbarkeit__sache='nachname') \
                     .filter(sichtbarkeit__bereich='bwinf', sichtbarkeit__sache='adresse') \
                     .filter(beitrittsdatum__lte=today) \
                     .exclude(austrittsdatum__lte=today)
-            
-        for mitglied in m:
-            f.write(mitglied.vorname + "," \
-                + mitglied.nachname + "," \
-                + mitglied.strasse + "," \
-                + mitglied.adresszusatz + "," \
-                + mitglied.plz + "," \
-                + mitglied.stadt + "," \
-                + mitglied.land + "\n")
+
+        csvwriter = csv.writer(f)
+
+        for mitglied in ms:
+            csvwriter.writerow([mitglied.vorname,
+                                mitglied.nachname,
+                                mitglied.strasse,
+                                mitglied.adresszusatz,
+                                mitglied.plz,
+                                mitglied.stadt,
+                                mitglied.land])
 
     return HttpResponse("OK")
