7c18a137a7b58e3a2f5dfee4f52c1cbf4f2bd5d6
Robert Czechowski <roczech+git@gmail.com>
2021-05-02 23:03:47 +0200

    Clean up auth method 'MitgliedsnummerBackend'

--- bwinfalumni/authentication_backends.py
+++ bwinfalumni/authentication_backends.py
@@ -1,38 +1,30 @@
 #from django.conf import settings
+from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth.hashers import check_password
 from django.contrib.auth.models import User
 
 from mitglieder.models import Mitglied
 
-class MitgliedsnummerBackend:
+class MitgliedsnummerBackend(ModelBackend):
     """
     Authentifiziert gegen die Mitgliedsnummer (und Passwort) statt gegen
     den Benutzernamen.
     """
 
-    def authenticate(self, request, username=None, password=None):
+    def authenticate(self, request, username=None, password=None, **kwargs):
         try:
             if username[0] == "m":
                 username = username[1:]
             userid = int(username)
         except:
-            return None ## Oder sollte das einen Fehler werfen?
-        
+            return None
+
         try:
             mitglied = Mitglied.objects.get(mitgliedsnummer = userid)
-            user = mitglied.benutzermitglied_set.first().benutzer
-            if check_password(password, user.password):
-                return user
-            else:
-                return None
-        except User.DoesNotExist:
+            for benutzermitglied in mitglied.benutzermitglied_set.all():
+                user = benutzermitglied.benutzer
+                if check_password(password, user.password):
+                    return user
             return None
-            # Hier könnte ein Benutzer angelegt werden. TODO: Wie?
-            user = User(username=username)
-            user.save()
-
-    def get_user(self, user_id):
-        try:
-            return User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return None
