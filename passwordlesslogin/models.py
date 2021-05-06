d6c93c821bf432b28404c192d40ff06d5bd7104d
Robert Czechowski <roczech+git@gmail.com>
2021-05-06 12:26:46 +0200

    Vollständige Implementierung des passwortlosen Login

--- passwordlesslogin/models.py
+++ passwordlesslogin/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
-from mitglieder.models import Mitglied
-
+from django.contrib.auth.models import User
 
 class Passwordlesslogincode(models.Model):
-
-    benutzer    = models.ForeignKey(Mitglied, on_delete=models.PROTECT)
+    benutzer    = models.ForeignKey(User, on_delete=models.PROTECT)
     logincode   = models.CharField(max_length=250)
-    valid_until = models.DateField()
+    valid_until = models.DateTimeField()
--- passwordlesslogin/views.py
+++ passwordlesslogin/views.py
@@ -1,11 +1,23 @@
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth import login
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from django.conf import settings
+from django import forms
 
 from .models import Passwordlesslogincode
+from mitglieder.models import Mitglied
+
+from random import choice
+from datetime import date, timedelta
+
+
+
 
 
 class EmailForm(forms.Form):
-    email = forms.EmailField(label='Neuer E-Mailadresse', max_length=250)
+    email = forms.EmailField(label='E-Mailadresse', max_length=250)
 
 def index(request):
     errormessage = ""
@@ -16,12 +28,27 @@ def index(request):
         if form.is_valid():
             email = form.cleaned_data['email']
 
-            # Finde User mit E-Mail
+            benutzer = None
 
-            # Versuche E-Mail zu senden
+            # Nimm erstbesten Benutzer von erstbestem Mitglied
+            for mitglied in Mitglied.objects.filter(email=email):
+                for benutzermitglied in mitglied.benutzermitglied_set.all():
+                    benutzer = benutzermitglied.benutzer
+                    break
 
-                successmessage = "E-Mail wurde an die angegebene Adresse versendet."
-                errormessage = "E-Mail konnte nicht gesendet werden."
+            # Überschreibe mit erstbestem Benutzer, wenn Benutzer mit E-Mail existiert
+            for user in User.objects.filter(email=email):
+                benutzer = user
+                break
+
+            if benutzer != None:
+                logincode = ''.join(choice("ABCDEFGHKMNPQRSTUVWXYZ23456789") for _ in range(32))
+                valid_until = timezone.now() + timedelta(hours=2) # Zwei Stunden gültig
+                Passwordlesslogincode.objects.create(benutzer=benutzer, logincode=logincode, valid_until=valid_until)
+                if sende_email_mit_logincode(email, logincode):
+                    successmessage = "E-Mail wurde an die angegebene Adresse versendet."
+                else:
+                    errormessage = "E-Mail konnte nicht gesendet werden."
             else:
                 errormessage = "E-Mailadresse nicht gefunden."
         else:
@@ -39,13 +66,36 @@ def index(request):
 def verifikation(request, code):
     try:
         logincode = Passwordlesslogincode.objects.get(logincode=code)
-        # if valid_until gültig
+        if logincode.valid_until > timezone.now():
             benutzer = logincode.benutzer
-            login(request, benutzer)
-            # redirect
+            login(request, benutzer, backend='django.contrib.auth.backends.ModelBackend')
+            return redirect('/')
         else:
             errormessage = "Dieser Link ist bereits abgelaufen."
-    except ObjectDoesNotExist: # Kein Login mit diesem Code vorhanden
+    except ObjectDoesNotExist:
         errormessage = "Dieser Link ist nicht gültig."
 
     return render(request, 'passwordlesslogin/verifikation.html',{'errormessage': errormessage,})
+
+
+def sende_email_mit_logincode(email, code):
+    with open(settings.BWINFALUMNI_LOGS_DIR + 'maillog', 'a', encoding='utf8') as f:
+        with open (settings.BWINFALUMNI_MAIL_TEMPLATE_DIR + 'logincode.txt', 'r', encoding='utf8') as templatefile:
+            template = ""
+            for line in templatefile.readlines():   # Remove first two character of every line if they are spaces
+                template += line[2:] if line[:2] == "  " else line   # Allows for templates in dokuwiki syntax …
+            data = {'code': code}
+            betreff = "E-Mail-Login BwInf Alumni und Freunde e. V.".format(**data)
+            text = template.format(**data)
+
+            try:
+                send_mail(betreff, text, 'vorstand@alumni.bwinf.de', [email])
+                f.write("Date: " + str(date.today()) + "\n")
+                f.write("To: " + mitgliedschaftsantrag.email + "\n")
+                f.write("From: vorstand@alumni.bwinf.de\n")
+                f.write("Subject: " + betreff + "\n\n")
+                f.write(text + "\n\n")
+                return True
+            except:
+                f.write("ERROR: Could not send mail to: " + email + "(" + str(date.today()) + ": " + betreff + ") (logincode: " + code + ")\n\n")
+    return False
