268a2b3ff18888c4b385353406435f0d40908a9e
Robert Czechowski <roczech+git@gmail.com>
2022-05-27 01:06:29 +0200

    Add routes to application 'kontostatistik'

--- bwinfalumni/urls.py
+++ bwinfalumni/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     url(r'^mitgliedschaftsantrag/', include('mitgliedschaftsantrag.urls', namespace='mitgliedschaftsantrag')),
     url(r'^mailinglistenadressen/', include('mailinglistenadressen.urls', namespace='mailinglistenadressen')),
     url(r'^mitgliederkarte/', include('mitgliederkarte.urls', namespace='mitgliederkarte')),
+    url(r'^kontostatistik/', include('kontostatistik.urls', namespace='kontostatistik')),
     url(r'^lastschriftmandate/', include('lastschriftmandatverwaltung.urls', namespace='lastschriftmandatverwaltung')),
     url(r'^logindaten/', include('emailbenutzername.urls', namespace='emailbenutzername')),
     url(r'^directlogin/', include('passwordlesslogin.urls', namespace='passwordlesslogin')),
