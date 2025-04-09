a19fe1f11b1c2aba34c9e382bf0fa92069f48281
Robert Czechowski <roczech+git@gmail.com>
2025-04-10 00:13:01 +0200

    MitgliedskontoBuchung: Neues Feld 'wirksam' um Spenden als unwirksam in die Mitgliedskonto-Buchungsübersicht mit aufzunehmen

--- kontostatistik/views.py
+++ kontostatistik/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 
 from mitglieder.models import Mitglied, MitgliedskontoBuchung, MitgliedskontoBuchungstyp
 
-
 @login_required
 @user_passes_test(lambda u: u.is_superuser or u.groups.filter(name='vorstand').exists())
 def index(request):
@@ -12,10 +11,9 @@ def index(request):
     values = {}
     for mitglied in all_mitglieder:
         val = 0
-        for buchung in mitglied.mitgliedskontobuchung_set.all().order_by('buchungsdatum'):
+        for buchung in mitglied.mitgliedskontobuchung_set.filter(wirksam=True):
             val += buchung.cent_wert
         values[mitglied.mitgliedsnummer] = {'mitglied': mitglied, 'betrag': val/100.0}
-
 
     value_list = [v for v in values.values()]
     value_list = sorted(value_list, key=lambda x: x['betrag'], reverse=False)
