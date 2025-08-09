07a911b8a60473d114660ce79346627d2919cb66
Robert Czechowski <robert+git@czecho.de>
2025-08-09 23:40:56 +0200

    Verein: Ermögliche Vereinsvorstand und Freistellungsbescheide einzutragen

--- verein/models.py
+++ verein/models.py
@@ -3,7 +3,9 @@ from datetime import date
 
 class Verein(models.Model):
 
-    mitgliedsbeitrag_cent = models.IntegerField()
+    mitgliedsbeitrag_cent = models.IntegerField(null=True, blank=True)
+    vorstand1             = models.CharField(max_length=250, blank=True)
+    vorstand2             = models.CharField(max_length=250, blank=True)
     beschlussfassung      = models.DateField(default=date.today)
 
     def __str__(self):
@@ -13,3 +15,18 @@ class Verein(models.Model):
         db_table = "verein"
         verbose_name = "Verein"
         verbose_name_plural = "Vereinsbeschlüsse"
+
+class Freistellungsbescheid(models.Model):
+
+    finanzamt    = models.CharField(max_length=250)
+    steuernummer = models.CharField(max_length=250)
+    zeitraum     = models.CharField(max_length=250)
+    datum        = models.DateField(default=date.today)
+
+    def __str__(self):
+        return "Freistellungsbescheid vom Finanzamt " + finanzamt + " (Steuernummer" + self.steuernummer + ") für den Zeitraum " + self.zeitraum + " wurde erteilt am " + str(self.datum) + "."
+
+    class Meta:
+        db_table = "freistellungsbescheid"
+        verbose_name = "Freistellungsbescheid"
+        verbose_name_plural = "Freistellungsbescheide"
--- verein/views.py
+++ verein/views.py
@@ -3,13 +3,18 @@ from django.shortcuts import render
 from django.forms import ModelForm
 from django.contrib.auth.decorators import login_required, user_passes_test
 
-from .models import Verein
+from .models import Verein, Freistellungsbescheid
 from mitglieder.models import Mitglied
 
 class VereinForm(ModelForm):
     class Meta:
         model = Verein
-        fields = ["beschlussfassung", "mitgliedsbeitrag_cent"]
+        fields = ["beschlussfassung", "vorstand1", "vorstand2", "mitgliedsbeitrag_cent"]
+
+class FreistellungsbescheidForm(ModelForm):
+    class Meta:
+        model = Freistellungsbescheid
+        fields = ["datum", "finanzamt", "steuernummer", "zeitraum"]
 
 @login_required
 @user_passes_test(lambda u: u.is_superuser or u.groups.filter(name='vorstand').exists())
@@ -17,12 +22,20 @@ def index(request):
     if request.method == 'POST':
         beschlussform = VereinForm(request.POST)
         if beschlussform.is_valid():
-            beschluss = beschlussform.save()
+            if beschlussform.cleaned_data['mitgliedsbeitrag_cent'] != None or beschlussform.cleaned_data['vorstand1'] != "" or beschlussform.cleaned_data['vorstand2'] != "":
+                beschluss = beschlussform.save()
 
-            if beschluss.mitgliedsbeitrag_cent != None:
-                Mitglied.objects.filter(foerdermitglied=False).update(beitrag_cent=beschluss.mitgliedsbeitrag_cent)
+                if beschluss.mitgliedsbeitrag_cent != None:
+                    Mitglied.objects.filter(foerdermitglied=False).update(beitrag_cent=beschluss.mitgliedsbeitrag_cent)
 
-    beschluesse = Verein.objects.all()
+        bescheidform = FreistellungsbescheidForm(request.POST)
+        if bescheidform.is_valid():
+            bescheid = bescheidform.save()
+
+    beschluesse = Verein.objects.order_by("beschlussfassung")
     beschluss = VereinForm()
 
-    return render(request, 'verein/index.html', {'beschluesse': beschluesse, 'beschluss': beschluss})
+    bescheide = Freistellungsbescheid.objects.order_by("datum")
+    bescheid = FreistellungsbescheidForm()
+
+    return render(request, 'verein/index.html', {'beschluesse': beschluesse, 'beschluss': beschluss, 'bescheide': bescheide, 'bescheid': bescheid})
