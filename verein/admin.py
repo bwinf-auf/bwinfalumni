e801a9115b4ff474555f8038000c2a0fe87dab33
Robert Czechowski <robert+git@czecho.de>
2025-08-10 18:53:07 +0200

    Add missing models to admin interface

--- verein/admin.py
+++ verein/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Verein
+from .models import Verein, Freistellungsbescheid
 
 admin.site.register(Verein)
+admin.site.register(Freistellungsbescheid)
