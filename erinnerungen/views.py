8ec3a7b5f7c24f5ef4c75416b8d37a1b3f177eb1
Robert Czechowski <robert+git@czecho.de>
2025-08-10 19:03:32 +0200

    Fix: Erinnerungen: Email-Versand

--- erinnerungen/views.py
+++ erinnerungen/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
 from django.forms import ModelForm
+from django.core.mail import send_mail
 from django.contrib.auth.decorators import login_required, user_passes_test
 
 from .models import Erinnerung, Dauer
